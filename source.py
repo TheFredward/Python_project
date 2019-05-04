2497898c06a674c380d08fab1529434456a94317
TheFredward <fventura59@gmail.com>
2019-05-03 21:54:24 -0500

    Cleared basic concepts

--- source.py
+++ source.py
@@ -1,8 +1,1 @@
 print('hello World! Time to learn a new language\n \n')
-# Request user name
-inputName = input('Enter a name: ')
-# display input
-print('\n')
-outName = inputName + ' Ready for this!?'
-print('\n')
-print(outName)
