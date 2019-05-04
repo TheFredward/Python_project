8c38a3d36cd9d2315e327ea933fa8dd24295d148
TheFredward <fventura59@gmail.com>
2019-05-03 21:53:03 -0500

    added tuples, arrays, and dictionaries

--- source.py
+++ source.py
@@ -4,5 +4,24 @@ inputName = input('Enter a name: ')
 # display input
 print('\n')
 outName = inputName + ' Ready for this!?'
-print('\n')
 print(outName)
+print('\n')
+# ------
+# Tuples NON MUTABLE: store variables but cannot modify, index access
+# Array and list: can modify elements, index access
+# Dictionaries: similar to tuples but modifiable, values are stored based on keys
+# ------
+# tuples
+size = (100, 200)
+# finding the index and storing in width
+width = size[0]
+# take note of the comma after
+new_size = size + (300,)
+# Array
+movement =  [5,-2,3,-4]
+movement.append('text')
+print(movement)
+# dictionary
+dictionaries = {'p0': 50, 'p2':23}
+print(dictionaries.keys())
+print(dictionaries['p2'])
