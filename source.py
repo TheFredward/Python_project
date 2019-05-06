3680c2ff21ff9e4655994e59c6f15449dce13a57
TheFredward <fventura59@gmail.com>
2019-05-05 21:38:36 -0500

    created a class for display

--- source.py
+++ source.py
@@ -1,31 +1,41 @@
 print('hello World! Time to learn a new language\n \n')
 import pygame
-# NOTE: after importing pygame, initialize pygame
-pygame.init()
 # constant global variables
 S_WIDTH = 500
 S_HEIGHT = 500
 BLUE_HUE = (29,109,194)
-END_DISPLAY = False
-
 # create an object to help maintain time using Clock
 time_interval = pygame.time.Clock()
-# Update the clock also refers to frame rate
-time_rate = 60 #fps
-# display blue hue by creating a display with specified WxH
-screen = pygame.display.set_mode((S_WIDTH,S_HEIGHT))
-screen.fill(BLUE_HUE)
-#name of the screen
-pygame.display.set_caption('First display Test')
+screen = 'First display Test'
+class StartScreen(object):
+    """docstring for StartScreen."""
+    # Update the clock also refers to frame rate
+    time_rate = 60 #fps
 
-while not END_DISPLAY:
-    # we get all events that are occurring, ie mouse clicks, keyboard strokes etc
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            END_DISPLAY = True
-    # as long as the while loop is running we need to show and update the display as well as keep up with the time using .tick()
-    pygame.display.update()
-    time_interval.tick(time_rate)
+    def __init__(self, mScreenTitle,mWidth,mHeight):
+        self.mScreenTitle = mScreenTitle
+        self.mWidth = mWidth
+        self.mHeight = mHeight
+        # display blue hue by creating a display with specified WxH
+        self.screen = pygame.display.set_mode((S_WIDTH,S_HEIGHT))
+        self.screen.fill(BLUE_HUE)
+        #name of the screen
+        pygame.display.set_caption(mScreenTitle)
+    def loadScreen(self):
+        END_DISPLAY = False
+        while not END_DISPLAY:
+            # we get all events that are occurring, ie mouse clicks, keyboard strokes etc
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    END_DISPLAY = True
+            # as long as the while loop is running we need to show and update the display as well as keep up with the time using .tick()
+            pygame.display.update()
+            time_interval.tick(self.time_rate)
+# NOTE: after importing pygame, initialize pygame
+pygame.init()
+
+newScreen = StartScreen(screen,S_WIDTH,S_HEIGHT)
+newScreen.loadScreen()
 # we need to end pygame and quit
 pygame.quit()
 quit()
