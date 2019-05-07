a3be6be53ad3b0a7c46d5f9af2d33c375469faea
TheFredward <fventura59@gmail.com>
2019-05-07 18:44:34 -0500

    class CreateButton initiated

--- tkinter_Window.py
+++ tkinter_Window.py
@@ -38,8 +38,13 @@ def buttonClick(numbers):
     return;
 
 def OKClick():
-
-    return;
+    # check that operator is not null
+    if operator is '':
+        level = Toplevel()
+        displayString = Label(level,text= operator, height = 800,width = 400)
+        displayString.pack()
+    else:
+        return;
 
 def DeleteClick():
     global operator
@@ -47,19 +52,61 @@ def DeleteClick():
     text_Input.set(operator)
     return;
 
-
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "1", bg = "gray", command = lambda: buttonClick("1")).grid(row=1, column=0, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "2", bg = "gray", command = lambda: buttonClick("2")).grid(row=1, column=1, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "3", bg = "gray", command = lambda: buttonClick("3")).grid(row=1, column=2, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "4", bg = "gray", command = lambda: buttonClick("4")).grid(row=2, column=0, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "5", bg = "gray", command = lambda: buttonClick("5")).grid(row=2, column=1, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "6", bg = "gray", command = lambda: buttonClick("6")).grid(row=2, column=2, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "7", bg = "gray", command = lambda: buttonClick("7")).grid(row=3, column=0, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "8", bg = "gray", command = lambda: buttonClick("8")).grid(row=3, column=1, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "9", bg = "gray", command = lambda: buttonClick("9")).grid(row=3, column=2, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "OK", bg = "gray", command = lambda: OKClick()).grid(row=4, column=0, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "0", bg = "gray", command = lambda: buttonClick("0")).grid(row=4, column=1, sticky="ew")
-okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "<-", bg = "gray", command = lambda: DeleteClick()).grid(row=4, column=2, sticky="ew")
-
-
+class CreateButton():
+    """Doc: Class that will be used to create numerical keys and later alphabet keys to display and save the values in dictionaries so that they can be refered back via keys (for the time being)"""
+    # local variables in create button class
+    padx = 20
+    pady = 16
+    bd = 4
+    fg = "black"
+    mFont = ('arial',15)
+    mBg = 'gray'
+    def __init__(self,frameNmbr,mText,mRow,mColumn):
+        self.frameNmbr = frameNmbr
+        self.mText = mText
+        self.mRow = mRow
+        self.mColumn = mColumn
+    def passButtonVal(self):
+        okayButton = Button(self.frameNmbr, padx = self.padx, pady = self.pady, bd = self.bd, fg = self.fg, font = self.mFont, text = self.mText, bg = self.mBg, command = lambda: buttonClick(self.mText)).grid(row=self.mRow, column = self.mColumn, sticky="ew")
+    def enterButton(self):
+        okayButton = Button(self.frameNmbr, padx = self.padx, pady = self.pady, bd = self.bd, fg = self.fg, font = self.mFont, text = self.mText, bg = self.mBg, command = lambda:  OKClick()).grid(row=self.mRow, column= self.mColumn, sticky="ew")
+    def deleteButton(self):
+        okayButton = Button(self.frameNmbr, padx = self.padx, pady = self.pady, bd = self.bd, fg = self.fg, font = self.mFont, text = self.mText, bg = self.mBg, command = lambda: DeleteClick()).grid(row= self.mRow, column= self.mColumn, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "1", bg = "gray", command = lambda: buttonClick("1")).grid(row=1, column=0, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "2", bg = "gray", command = lambda: buttonClick("2")).grid(row=1, column=1, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "3", bg = "gray", command = lambda: buttonClick("3")).grid(row=1, column=2, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "4", bg = "gray", command = lambda: buttonClick("4")).grid(row=2, column=0, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "5", bg = "gray", command = lambda: buttonClick("5")).grid(row=2, column=1, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "6", bg = "gray", command = lambda: buttonClick("6")).grid(row=2, column=2, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "7", bg = "gray", command = lambda: buttonClick("7")).grid(row=3, column=0, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "8", bg = "gray", command = lambda: buttonClick("8")).grid(row=3, column=1, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "9", bg = "gray", command = lambda: buttonClick("9")).grid(row=3, column=2, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "OK", bg = "gray", command = lambda: OKClick()).grid(row=4, column=0, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "0", bg = "gray", command = lambda: buttonClick("0")).grid(row=4, column=1, sticky="ew")
+# okayButton = Button(FrameThree, padx=20, pady=16, bd = 4, fg = "black", font = ('arial', 15), text = "<-", bg = "gray", command = lambda: DeleteClick()).grid(row=4, column=2, sticky="ew")
+# initialize all buttons using CreateButton class and pass the necessary values that are needed
+okayButton1 = CreateButton(FrameThree,"1",1,0)
+okayButton2 = CreateButton(FrameThree,"2",1,1)
+okayButton3 = CreateButton(FrameThree,"3",1,2)
+okayButton4 = CreateButton(FrameThree,"4",2,0)
+okayButton5 = CreateButton(FrameThree,"5",2,1)
+okayButton6 = CreateButton(FrameThree,"6",2,2)
+okayButton7 = CreateButton(FrameThree,"7",3,0)
+okayButton8 = CreateButton(FrameThree,"8",3,1)
+okayButton9 = CreateButton(FrameThree,"9",3,2)
+okayButton = CreateButton(FrameThree,"OK",4,0)
+okayButton0 = CreateButton(FrameThree,"0",4,1)
+okayButton_bckspc = CreateButton(FrameThree,"<-",4,2)
+okayButton1.passButtonVal()
+okayButton2.passButtonVal()
+okayButton3.passButtonVal()
+okayButton4.passButtonVal()
+okayButton5.passButtonVal()
+okayButton6.passButtonVal()
+okayButton7.passButtonVal()
+okayButton8.passButtonVal()
+okayButton9.passButtonVal()
+okayButton.enterButton()
+okayButton0.passButtonVal()
+okayButton_bckspc.deleteButton()
 window.mainloop()
