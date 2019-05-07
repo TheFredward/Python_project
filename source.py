4f10e6787fe9754088c53c87c6f7c76b0b9b837c
TheFredward <fventura59@gmail.com>
2019-05-06 22:35:24 -0500

    able to take user input and clear it

--- source.py
+++ source.py
@@ -3,6 +3,7 @@ import pygame
 # constant global variables
 S_WIDTH = 500
 S_HEIGHT = 500
+BLACK = (255,255,255)
 BLUE_HUE = (29,109,194)
 # create an object to help maintain time using Clock
 time_interval = pygame.time.Clock()
@@ -11,7 +12,8 @@ class StartScreen(object):
     """docstring for StartScreen."""
     # Update the clock also refers to frame rate
     time_rate = 60 #fps
-
+    # global variable to hold in the user input
+    user_input = ''
     def __init__(self, mScreenTitle,mWidth,mHeight):
         self.mScreenTitle = mScreenTitle
         self.mWidth = mWidth
@@ -23,11 +25,25 @@ class StartScreen(object):
         pygame.display.set_caption(mScreenTitle)
     def loadScreen(self):
         END_DISPLAY = False
+        font = pygame.font.Font(None,50)
         while not END_DISPLAY:
             # we get all events that are occurring, ie mouse clicks, keyboard strokes etc
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     END_DISPLAY = True
+                    # check if the event is a keystroke
+                elif event.type == pygame.KEYDOWN:
+                    # check if the keystroke is a alphabet and if it is then store to user_input
+                    if event.unicode.isalpha():
+                        self.user_input += event.unicode
+                    # if enter is pressed clear user input then refill the screen with the same color
+                    elif event.key == pygame.K_RETURN:
+                        self.user_input = ''
+                        self.screen.fill(BLUE_HUE)
+            # using the font from pygame render the input from the user
+            block = font.render(self.user_input,True,BLACK)
+            inputBox = block.get_rect()
+            self.screen.blit(block,inputBox)
             # as long as the while loop is running we need to show and update the display as well as keep up with the time using .tick()
             pygame.display.update()
             time_interval.tick(self.time_rate)
