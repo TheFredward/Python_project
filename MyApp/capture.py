f7665c17175f3b38d34bd63328b0f0f0ceb7f2d6
TheFredward <fventura59@gmail.com>
2019-05-29 12:28:06 -0500

    created an error message for camera

--- MyApp/capture.py
+++ MyApp/capture.py
@@ -1,9 +1,23 @@
 import cv2, time
+# import tkinter as well as messagebox to display ErrorLabel
+import tkinter as tk
+from tkinter.messagebox import showinfo
 
 class capture():
 
     def __init__(self):
+        # initalize camera
         self.video=cv2.VideoCapture(0)
+        # create a wndow to display error, using topLevel to show it on top of the mainApp
+        win = tk.Toplevel()
+        win.wm_title("Error")
+        l = tk.Label(win, text='ErrorLabel')
+        # display in the center of the mainApp screen
+        l.grid(row=0, column=0)
+
+        # Determine if the camera is available\
+        if self.video is None or not self.video.isOpened():
+            showinfo("Error","Camera not available, use other credentials" )
 
     def get_frame(self):
         a=0
